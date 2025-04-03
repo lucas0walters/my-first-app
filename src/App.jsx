b74c01f377444eeb3aab4cdae9aca3a316611d95
lucas0walters <lucas.o.walters@gmail.com>
2025-04-03 12:44:31 -0400

    Task 1: Create a New Component

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Your Mom...</h1>
+      <h1>Edited heading</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
